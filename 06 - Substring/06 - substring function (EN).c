71ad474cadfa770a8b5a99d24d4bea7db74391da
Vinicius Romera C <viniciusromera@hotmail.com>
2025-04-02 14:07:16 -0300

    Delete 06 - Substring/06 - substring function (EN).c

--- 06 - Substring/06 - substring function (EN).c
+++
@@ -1,8 +0,0 @@
-void substring (char origin[], char destiny[], int start, int end){
-
-    int j=0;
-    for (int i=start; i<=end ; i++, j++){
-        destiny[j]=origin[i];
-    }
-    destiny[j]='\0';
-}
